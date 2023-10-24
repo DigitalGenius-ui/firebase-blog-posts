c25241e762f819f2e6fbf1b16f0b5f0d77363101
DigitalGenius-ui <miladmarketing282@gmail.com>
2023-10-24 16:22:36 +0800

    search bar has been added

--- src/components/Home/Header/HomeHeader.jsx
+++ src/components/Home/Header/HomeHeader.jsx
@@ -11,6 +11,7 @@ import UserModal from "./UserModal";
 
 const HomeHeader = () => {
   const [modal, setModal] = useState(false);
+  const [searchModal, setSearchModal] = useState(false);
   return (
     <header className="border-b border-gray-200">
       <div className="size h-[60px] flex items-center justify-between">
@@ -21,10 +22,15 @@ const HomeHeader = () => {
               <BsMedium />
             </span>
           </Link>
-          <Search />
+          <Search modal={searchModal} setModal={setSearchModal} />
         </div>
         {/* right side  */}
         <div className="flex items-center gap-3 sm:gap-7">
+          <span
+            onClick={() => setSearchModal(true)}
+            className="flex sm:hidden text-3xl text-gray-300 cursor-pointer">
+            <CiSearch />
+          </span>
           <Link
             to="/write"
             className="hidden md:flex items-center gap-1 text-gray-500">
--- src/components/Home/Header/Search.jsx
+++ src/components/Home/Header/Search.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { CiSearch } from "react-icons/ci";
+import Modal from "../../../utils/Modal";
 
-const Search = () => {
-  return <div>Search</div>;
+const Search = ({ modal, setModal }) => {
+  return (
+    <>
+      <Modal modal={modal} setModal={setModal}>
+        <div
+          className={`absolute sm:relative right-4 left-4 top-[4rem] sm:left-0 sm:top-0
+          ${modal ? "visible opacity-100" : "invisible opacity-0"}
+          transition-all duration-100`}>
+          <div className="flex items-center gap-1 bg-gray-100 px-2 rounded-full relative z-10">
+            <span className="text-2xl text-gray-400">
+              <CiSearch />
+            </span>
+            <input
+              className="bg-transparent outline-none py-[0.7rem] text-sm w-full"
+              type="text"
+              placeholder="Search Medium"
+            />
+          </div>
+        </div>
+      </Modal>
+    </>
+  );
 };
 
 export default Search;
