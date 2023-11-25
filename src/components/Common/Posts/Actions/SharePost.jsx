b74d899e421cc7911ec5857f021abbfb7b66a5d3
DigitalGenius-ui <miladmarketing282@gmail.com>
2023-11-25 17:35:17 +0800

    share and copy link has been implemented

--- src/components/Common/Posts/Actions/SharePost.jsx
+++ src/components/Common/Posts/Actions/SharePost.jsx
@@ -12,27 +12,46 @@ import {
   BiLogoTwitter,
   BiLogoLinkedinSquare,
 } from "react-icons/bi";
+import { toast } from "react-toastify";
 
 const SharePost = () => {
   const [showDrop, setShowDrop] = useState(false);
+  const path = window.location.href;
+
+  const copyLink = async () => {
+    try {
+      await navigator.clipboard.writeText(path);
+      toast.success("Link has been copied");
+      setShowDrop(false);
+    } catch (error) {
+      toast.error(error.message);
+      setShowDrop(false);
+    }
+  };
   return (
     <div className="relative">
       <button onClick={() => setShowDrop(!showDrop)}>
         <CiShare1 className="text-2xl" />
       </button>
       <DropDown showDrop={showDrop} setShowDrop={setShowDrop} size="w-[12rem]">
-        <Button click="" title="Copy Link" icon={<BiLink />} />
-        <Button click="" title="Share On Twitter" icon={<BiLogoTwitter />} />
-        <Button
-          click=""
-          title="Share On Facebook"
-          icon={<BiLogoFacebookCircle />}
-        />
-        <Button
-          click=""
-          title="Share On LinkedIn"
-          icon={<BiLogoLinkedinSquare />}
-        />
+        <Button click={copyLink} title="Copy Link" icon={<BiLink />} />
+        <TwitterShareButton url={path}>
+          <Button click="" title="Share On Twitter" icon={<BiLogoTwitter />} />
+        </TwitterShareButton>
+        <FacebookShareButton url={path}>
+          <Button
+            click=""
+            title="Share On Facebook"
+            icon={<BiLogoFacebookCircle />}
+          />
+        </FacebookShareButton>
+        <LinkedinShareButton url={path}>
+          <Button
+            click=""
+            title="Share On LinkedIn"
+            icon={<BiLogoLinkedinSquare />}
+          />
+        </LinkedinShareButton>
       </DropDown>
     </div>
   );
@@ -43,8 +62,9 @@ export default SharePost;
 const Button = ({ click, icon, title }) => {
   return (
     <button
+      onClick={click}
       className="p-2 hover:bg-gray-200 hover:text-black/80 w-full text-sm text-left
-    flex items-center gap-2 cursor-pointer text-gray-500">
+      flex items-center gap-2 cursor-pointer text-gray-500">
       <span className="text-[1.2rem]">{icon}</span>
       {title}
     </button>
