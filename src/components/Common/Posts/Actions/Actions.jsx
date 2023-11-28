86caa212e13ed873d7368e71f641eb3aee9b4d57
DigitalGenius-ui <miladmarketing282@gmail.com>
2023-11-28 14:50:18 +0800

    action comp is added

--- src/components/Common/Posts/Actions/Actions.jsx
+++ src/components/Common/Posts/Actions/Actions.jsx
@@ -3,9 +3,12 @@ import { BsThreeDots } from "react-icons/bs";
 import DropDown from "../../../../utils/DropDown";
 import { useNavigate } from "react-router-dom";
 import { Blog } from "../../../../Context/Context";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "../../../../firebase/firebase";
+import { toast } from "react-toastify";
 
 const Actions = ({ postId, title, desc }) => {
-  const { setUpdateData } = Blog();
+  const { setUpdateData, currentUser } = Blog();
   const [showDrop, setShowDrop] = useState(false);
   const handleClick = () => {
     setShowDrop(!showDrop);
@@ -17,6 +20,29 @@ const Actions = ({ postId, title, desc }) => {
     navigate(`/editPost/${postId}`);
     setUpdateData({ title, description: desc });
   };
+
+  const handleRemove = async () => {
+    try {
+      const ref = doc(db, "posts", postId);
+      const likeRef = doc(db, "posts", postId, "likes", currentUser?.uid);
+      const commentRef = doc(db, "posts", postId, "comments", currentUser?.uid);
+      const savedPostRef = doc(
+        db,
+        "users",
+        currentUser?.uid,
+        "savedPost",
+        postId
+      );
+      await deleteDoc(ref);
+      await deleteDoc(likeRef);
+      await deleteDoc(commentRef);
+      await deleteDoc(savedPostRef);
+      toast.success("post has been removed");
+      navigate("/");
+    } catch (error) {
+      toast.success(error.message);
+    }
+  };
   return (
     <div className="relative">
       <button onClick={handleClick}>
@@ -24,7 +50,7 @@ const Actions = ({ postId, title, desc }) => {
       </button>
       <DropDown showDrop={showDrop} setShowDrop={setShowDrop} size="w-[7rem]">
         <Button click={handleEdit} title="Edit Story" />
-        <Button click={() => console.log("delete")} title="Delete Story" />
+        <Button click={handleRemove} title="Delete Story" />
       </DropDown>
     </div>
   );
