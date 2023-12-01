e1fa91d3cd8394071151972cf5f346ac56dfd3f5
DigitalGenius-ui <miladmarketing282@gmail.com>
2023-12-01 17:15:35 +0800

    search bar is added

--- src/components/Home/Header/Search.jsx
+++ src/components/Home/Header/Search.jsx
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CiSearch } from "react-icons/ci";
 import Modal from "../../../utils/Modal";
+import { Blog } from "../../../Context/Context";
+import { useNavigate } from "react-router-dom";
 
 const Search = ({ modal, setModal }) => {
+  const [search, setSearch] = useState("");
+  const { postData } = Blog();
+
+  const searchData =
+    postData &&
+    postData?.filter((post) =>
+      post.title.toLowerCase().includes(search.toLowerCase())
+    );
+
+  const navigate = useNavigate();
   return (
     <>
       <Modal modal={modal} setModal={setModal}>
@@ -19,10 +31,39 @@ const Search = ({ modal, setModal }) => {
               <CiSearch />
             </span>
             <input
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
               className="bg-transparent outline-none py-[0.7rem] text-sm w-full"
               type="text"
               placeholder="Search Medium"
             />
+            {search !== "" && (
+              <div className="absolute right-0 left-0 top-full bg-white shadow rounded-md">
+                {searchData.length > 0 ? (
+                  <>
+                    {searchData.map((post, i) => (
+                      <div
+                        key={i}
+                        onClick={() => {
+                          navigate(`/post/${post?.id}`);
+                          setSearch("");
+                        }}
+                        className="p-2 border-b border-gray-300 hover:bg-gray-100 cursor-pointer">
+                        <h2 className="line-clamp-1 capitalize text-sm font-bold">
+                          {post.title}
+                        </h2>
+                        <div
+                          className="text-xs text-gray-500 line-clamp-2"
+                          dangerouslySetInnerHTML={{ __html: post.desc }}
+                        />
+                      </div>
+                    ))}
+                  </>
+                ) : (
+                  <p className="text-sm text-gray-500 p-3">No Post Found</p>
+                )}
+              </div>
+            )}
           </div>
         </div>
       </Modal>
